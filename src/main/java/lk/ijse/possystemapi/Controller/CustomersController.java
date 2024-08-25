e7a750ee82620e4718296ff7da680b64395b064b
UvinduV <uvindusanketh470@gmail.com>
2024-08-25 16:14:26 +0530

    Customer Save in db

--- src/main/java/lk/ijse/possystemapi/Controller/CustomersController.java
+++ src/main/java/lk/ijse/possystemapi/Controller/CustomersController.java
@@ -11,10 +11,29 @@ import lk.ijse.possystemapi.dto.CustomerDTO;
 import lk.ijse.possystemapi.dto.ItemDTO;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.UUID;
 
 @WebServlet(urlPatterns = "/Customer")
 public class CustomersController extends HttpServlet {
+    private Connection connection;
+    static String SAVE_CUSTOMER = "INSERT INTO customer (CustId,CustName,CustAddress,CustContact) VALUES (?,?,?,?)";
+
+    @Override
+    public void init() throws ServletException {
+        try {
+            var driverCalss = getServletContext().getInitParameter("driver-class");
+            var dbUrl = getServletContext().getInitParameter("dbURL");
+            var userName = getServletContext().getInitParameter("dbUserName");
+            var password = getServletContext().getInitParameter("dbPassword");
+            Class.forName(driverCalss);
+            this.connection =  DriverManager.getConnection(dbUrl,userName,password);
+        }catch (ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+        }
+    }
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
@@ -30,6 +49,23 @@ public class CustomersController extends HttpServlet {
         CustomerDTO customerDTO = jsonb.fromJson(req.getReader(), CustomerDTO.class);
         customerDTO.setId(id);
         System.out.println(customerDTO);
+
+        try {
+            var ps = connection.prepareStatement(SAVE_CUSTOMER);
+            ps.setString(1, customerDTO.getId());
+            ps.setString(2, customerDTO.getName());
+            ps.setString(3, customerDTO.getAddress());
+            ps.setString(4, customerDTO.getContact());
+
+            if(ps.executeUpdate() != 0){
+                resp.getWriter().write("Customer Saved");
+            }else {
+                resp.getWriter().write("customer Not Saved");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
